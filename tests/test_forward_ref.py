246fcd0cec053d22edf88d5c385a4170249f3869
euri10 <benoit.barthelet@gmail.com>
2025-05-08 15:39:23 +0200

    Revert "tests: parametrized loaders with supported classes/models"
    This reverts commit da4f8eff13b39f70378d5377aba7ad64b9485c2c.

--- tests/test_forward_ref.py
+++ tests/test_forward_ref.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
+from attr import dataclass as attr_dataclass
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass as pydantic_dataclass
+
 from dataclass_settings import load_settings
-from tests.utils import (
-    AttrConfig,
-    AttrFoo,
-    DataclassConfig,
-    DataclassFoo,
-    PDataclassConfig,
-    PDataclassFoo,
-    PydanticConfig,
-    PydanticFoo,
-)
+
+
+class PydanticConfig(BaseModel):
+    foo: PydanticFoo
+
+
+class PydanticFoo(BaseModel):
+    foo: int = 0
 
 
 def test_pydantic():
@@ -18,14 +22,44 @@ def test_pydantic():
     assert config == PydanticConfig(foo=PydanticFoo(foo=0))
 
 
+@dataclass
+class DataclassConfig:
+    foo: DataclassFoo
+
+
+@dataclass
+class DataclassFoo:
+    foo: int = 0
+
+
 def test_dataclass():
     config = load_settings(DataclassConfig)
     assert config == DataclassConfig(foo=DataclassFoo(foo=0))
 
 
+@pydantic_dataclass
+class PDataclassConfig:
+    foo: PDataclassFoo
+
+
+@pydantic_dataclass
+class PDataclassFoo:
+    foo: int = 0
+
+
 def test_pydantic_dataclass():
     config = load_settings(PDataclassConfig)
     assert config == PDataclassConfig(foo=PDataclassFoo(foo=0))
+
+
+@attr_dataclass
+class AttrConfig:
+    foo: AttrFoo
+
+
+@attr_dataclass
+class AttrFoo:
+    foo: int = 0
 
 
 def test_attr_dataclass():
--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,8 @@
-from __future__ import annotations
-
 import contextlib
 import sys
-from dataclasses import dataclass
 from unittest.mock import mock_open, patch
 
 import pytest
-from attr import dataclass as attr_dataclass
-from pydantic import BaseModel
-from pydantic.dataclasses import dataclass as pydantic_dataclass
 
 
 @contextlib.contextmanager
@@ -31,41 +25,3 @@ def env_setup(env={}, files={}):
 
 def skip_under(major: int, minor: int, *, reason: str):
     return pytest.mark.skipif(sys.version_info < (major, minor), reason=reason)
-
-
-class PydanticConfig(BaseModel):
-    foo: PydanticFoo
-
-
-class PydanticFoo(BaseModel):
-    foo: int = 0
-
-
-@dataclass
-class DataclassConfig:
-    foo: DataclassFoo
-
-
-@dataclass
-class DataclassFoo:
-    foo: int = 0
-
-
-@pydantic_dataclass
-class PDataclassConfig:
-    foo: PDataclassFoo
-
-
-@pydantic_dataclass
-class PDataclassFoo:
-    foo: int = 0
-
-
-@attr_dataclass
-class AttrConfig:
-    foo: AttrFoo
-
-
-@attr_dataclass
-class AttrFoo:
-    foo: int = 0
